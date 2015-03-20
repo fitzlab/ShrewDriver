1b8a99b9f33a0366908341be00e2f7565db05817
Theo Walker <naroom@gmail.com>
2015-03-20 04:07:00 -0400

    Better record keeping, more cleanup

--- ShrewDriver/LogAnalysis.py
+++ ShrewDriver/LogAnalysis.py
@@ -1,169 +1,40 @@
 from __future__ import division
-import fileinput, re
+import fileinput, re, math, sys
 
-dirPath = "C:/Users/fitzlab1/Desktop/analysis/"
+from util.Stats import dPrime, criterion
 
-filePath = dirPath + "Mercury_2014-12-26_1_log.txt"
+sys.path.append("./trial")
+from Trial import Trial
 
-def concatType(type1, type2):
-    return type1 + ' + ' + type2
+from Analyzer import Analyzer
 
-def makeStateHistograms(stateLickTimes, grayScreenTypes):
-    win = pg.GraphicsWindow()
-    win.resize(800,800)
-    win.setWindowTitle('Licks during each gray screen state')
+sys.path.append("./global")
+from Constants import *
 
-    plots = []
-    plots[0] = win.addPlot(row=1,col=1, title=grayScreenTypes[0])
-    plots[1] = win.addPlot(row=1,col=2, title=grayScreenTypes[1]) 
-
-    plots[2] = win.addPlot(row=2,col=1, title=grayScreenTypes[2])
-    plots[3] = win.addPlot(row=3,col=1, title=grayScreenTypes[3])
-
-    plots[4] = win.addPlot(row=2,col=2, title=grayScreenTypes[4])
-    plots[5] = win.addPlot(row=3,col=2, title=grayScreenTypes[5])
+if __name__ == '__main__':
+    #do offline analysis
+    taskErrorRates = []
+    mLPerHours = []
+    successRates = []
+    sPlusResponseRates = []
     
-    for i in range(0,len(grayScreenTypes)):
-        t = 1
-        if i==0: #DELAY
-            t = 2
-        y,x = np.histogram(stateLickTimes[grayScreenTypes[i]], bins=np.linspace(0, t, t/0.1))
-        curve = pg.PlotCurveItem(x, y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
-        plots[i].addItem(curve)
-
-# These are the different things that can happen in a successful trial
-hintTypes = ['Hint','No Hint']
-distractorTypes = ['No Distractors', 'One Distractor', 'Two Distractors']
-
-# set up results data structure 
-successCounts = {}
-for d in distractorTypes:
-    for h in hintTypes:
-        trialType = concatType(h, d)
-        successCounts[trialType] = 0
-
-# If it failed, let's find what state it failed at
-stateSet = ['TIMEOUT', 'INIT', 'DELAY', 'GRAY', 'SPLUS', 'SMINUS', 'REWARD']
-failCounts = {}
-for s in stateSet:
-    failCounts[s] = 0
-
-stateLickTimes = {} #for building histograms of lick times during each gray screen state
-grayScreenTypes = ['DELAY', 'GRAY', 'REWARD_HINT', 'REWARD_NOHINT', 'D_REWARD_HINT', 'D_REWARD_NOHINT', ]
-for s in grayScreenTypes:
-    stateLickTimes[s] = []
-
-
-abortCount = 0
-noResponseCount = 0
-
-prevState = '' #what state we were in, for labeling failure types
-prevAction = '' #The last result-relevant thing the shrew did -- lick or leave.
-
-wasSuccess = False
-hint = False
-numDistractors = 0
-
-trialStartTime = 0
-stateStartTime = 0
-
-totalReward = 0 #in mL
-
-#Run through log file 
-trialNum = 1 
-p = re.compile('\d+')
-for line in fileinput.input(filePath):
-    if re.search('State', line):
-        m = p.findall(line)
-        stateStartTime = float(m[1] + "." + m[2])
-        if int(m[0]) == 0:
-            if not wasSuccess:
-                #this was a failure, abort, or no response, because 
-                # we went to state 0 and it wasn't after a reward
-                if prevAction == 'lick':
-                    #failure
-                    failCounts[prevState] += 1
-                    print str(trialNum)  + " FAIL"
-                elif prevAction == 'leave':
-                    #aborts
-                    abortCount += 1
-                    print str(trialNum)  + " ABORT"
-                else:
-                    #no response
-                    noResponseCount += 1
-            else: 
-                print str(trialNum)  + " HIT " + str(m[1])
-            #new trial, so reset variables
-            hint = False
-            numDistractors = 0
-            wasSuccess = False
-            trialNum += 1
-            
-        if stateSet[int(m[0])] == "SMINUS":
-            numDistractors += 1
+    dirPath = "C:/Users/theo/Desktop/ShrewDriver/analysis/yep/"
+    import glob
+    import os
+    os.chdir(dirPath)
+    shrewName = 'other'
+    if dirPath.lower().find("chico") > -1:
+        shrewName= "Chico"
+    for file in glob.glob("*.txt"):
+        a = Analyzer(shrew=shrewName)
+        filePath = dirPath + file
         
-        prevAction = ''
-        #record what state that was
-        prevState = stateSet[int(m[0])]
+        a.readFile(filePath)
         
-    if re.search('RH', line):
-        #Success! Record what kind of trial that was.
-        trialType = ''
-        distractorType = distractorTypes[numDistractors]
-        if hint:
-            trialType = concatType(hintTypes[0], distractorType)
-        else:
-            trialType = concatType(hintTypes[1], distractorType)
-        successCounts[trialType] += 1
-        wasSuccess = True
-    if re.search('RL', line):
-        hint = True
-    if re.search('Io', line):
-        prevAction = 'leave'
-    if re.search('Lx', line):
-        prevAction = 'lick'
+        msg = a.getSummaryResults()
+        msg += a.getDiscriminationPerformance()
+        msg += a.getTaskErrors()
         
-        if prevState == "GRAY":# and not hint:
-            m = p.findall(line)
-            tTimeout = float(m[0] + "." + m[1])
-    
-    if re.search('bolus', line):
-        m = p.findall(line)
-        bolusSize = float(m[0] + "." + m[1])
-        totalReward += bolusSize
-
-completedTrials = 0
-
-
-#other
-print "===="
-print 'Abort: ' + str(abortCount)
-print 'No Response: ' + str(noResponseCount)
-
-#failure breakdown
-numFailures = 0
-print "===="
-print "FAILURES:"
-for key in stateSet:
-    print key + ' ' + str(failCounts[key])
-    numFailures += failCounts[key]
-print "Total Failures: " + str(numFailures)
-
-#success breakdown
-numSuccesses = 0
-print "===="
-print "HITS:"
-for key in successCounts.keys():
-    if successCounts[key] > 0:
-        print key + ' ' + str(successCounts[key])
-        numSuccesses += successCounts[key]
-print "Total Successes: " + str(numSuccesses)
-
-#summary
-print "===="
-numTrials = numFailures + numSuccesses
-successPercent = numSuccesses / (numFailures + numSuccesses) * 100
-print "Success rate: " + str(round(successPercent,1)) + "% (" + str(numSuccesses) + "/" + str(numTrials) + ")" 
-print 'Total Reward: ' + str(totalReward) + " mL"
-print filePath
-
+        print msg
+        
+        print filePath + "\n\n\n\n\n"
