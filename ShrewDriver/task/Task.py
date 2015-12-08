9601b2abc96272eeeb1a6551ccbc8e74bd2ceb1f
Theo Walker <naroom@gmail.com>
2015-12-08 16:58:43 -0500

    minor training

--- ShrewDriver/task/Task.py
+++ ShrewDriver/task/Task.py
@@ -350,7 +350,7 @@ class Task(object):
             self.hintChance = 0.0 #chance of sending a low reward at the start of the reward period
             
             self.hintBolus = 0.05 #0.03 is a good amount; just enough that the shrew will notice it but not enough to be worth working for on its own.
-            self.rewardBolus = 0.200
+            self.rewardBolus = 0.250
             self.rewardBolusHardTrial = 0.350
             
             #stimbot setup, including command strings for each state
