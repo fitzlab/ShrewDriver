8b782fc398da9f39f1d385d0c1f33e6012647f41
unknown <fitzlab1@mpfiwdtfitz74.mpfi.org>
2016-01-04 15:38:00 -0500

    yep

--- ShrewDriver/task/Task.py
+++ ShrewDriver/task/Task.py
@@ -347,7 +347,7 @@ class Task(object):
             self.grayDuration = 1
             self.rewardPeriod = self.grayDuration #needs to be no longer than gray duration!
             
-            self.hintChance = 0.25 #chance of sending a low reward at the start of the reward period
+            self.hintChance = 0.0 #chance of sending a low reward at the start of the reward period
             
             self.hintBolus = 0.05 #0.03 is a good amount; just enough that the shrew will notice it but not enough to be worth working for on its own.
             self.rewardBolus = 0.350
@@ -389,7 +389,7 @@ class Task(object):
             self.grayDuration = 1
             self.rewardPeriod = self.grayDuration #needs to be no longer than gray duration!
             
-            self.hintChance = 0.25 #chance of sending a low reward at the start of the reward period
+            self.hintChance = 0.0 #chance of sending a low reward at the start of the reward period
             
             self.hintBolus = 0.05 #0.03 is a good amount; just enough that the shrew will notice it but not enough to be worth working for on its own.
             self.rewardBolus = 0.300
